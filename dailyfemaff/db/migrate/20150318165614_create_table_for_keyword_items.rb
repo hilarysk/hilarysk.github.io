331baa1369bbbe6906434436a593498bd9778343
Hilary Stohs-Krause <hilarysk@gmail.com>
2015-03-30 16:55:37 -0500

    removes everything

--- dailyfemaff/db/migrate/20150318165614_create_table_for_keyword_items.rb
+++
@@ -1,9 +0,0 @@
-class CreateTableForKeywordItems < ActiveRecord::Migration
-  def change
-    create_table :keyword_items do |t|
-     t.integer :keyword_id
-     t.integer :item_id
-     t.text :item_type
-    end
-  end
-end
--- dailyfemaff/db/migrate/20150325182408_add_active_field_to_users_table.rb
+++
@@ -1,5 +0,0 @@
-class AddActiveFieldToUsersTable < ActiveRecord::Migration
-  def change
-    add_column :users, :status, :string, default: "active"
-  end
-end
